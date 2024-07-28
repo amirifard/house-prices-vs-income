09fbc6c78eb865cdbcd3d31c0e8dae4e99aa6d63
HOSS <amirifard@gmail.com>
2024-07-28 11:26:32 +0800

    load data

--- script.js
+++ script.js
@@ -1,101 +1,169 @@
 d3.csv('RHPI.csv').then(dataRHPI => {
     d3.csv('RPDI.csv').then(dataRPDI => {
-        initializeScene1(dataRHPI);
-        initializeScene2(dataRPDI);
-        initializeScene3(dataRHPI, dataRPDI);
+        // Use only the most recent quarter for each dataset
+        const latestRHPI = dataRHPI[dataRHPI.length - 1];
+        const latestRPDI = dataRPDI[dataRPDI.length - 1];
+
+        // Extract country names and values
+        const countries = Object.keys(latestRHPI).filter(key => key !== 'Quarter');
+        const rhpiData = countries.map(country => ({ country, value: +latestRHPI[country] }));
+        const rpdiData = countries.map(country => ({ country, value: +latestRPDI[country] }));
+
+        initializeScene1(rhpiData);
+        initializeScene2(rpdiData);
+        initializeScene3(rhpiData, rpdiData);
     });
 });
 
 function initializeScene1(data) {
-    const svg = d3.select("#scene1").append("svg").attr("width", 800).attr("height", 600);
-    // Sort data by RHPI
-    data.sort((a, b) => a.RHPI - b.RHPI);
-    // Create bars
-    // Animate timeline
+    const svg = d3.select("#scene1").append("svg");
+    const margin = {top: 20, right: 30, bottom: 40, left: 90},
+          width = 800 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+
+    const g = svg.append("g")
+                 .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    data.sort((a, b) => a.value - b.value);
+
+    const x = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.value)])
+                .range([0, width]);
+
+    const y = d3.scaleBand()
+                .domain(data.map(d => d.country))
+                .range([0, height])
+                .padding(0.1);
+
+    g.append("g")
+     .attr("class", "x axis")
+     .attr("transform", `translate(0,${height})`)
+     .call(d3.axisBottom(x));
+
+    g.append("g")
+     .attr("class", "y axis")
+     .call(d3.axisLeft(y));
+
     let index = 0;
     const interval = setInterval(() => {
         if (index < data.length) {
             const d = data[index];
-            svg.append("rect")
-                .attr("x", 100)
-                .attr("y", index * 20)
-                .attr("width", d.RHPI * 10)
-                .attr("height", 15)
-                .attr("fill", "steelblue");
-            svg.append("text")
-                .attr("x", 10)
-                .attr("y", index * 20 + 12)
-                .text(d.Country);
+            g.append("rect")
+             .attr("x", x(0))
+             .attr("y", y(d.country))
+             .attr("width", x(d.value))
+             .attr("height", y.bandwidth())
+             .attr("fill", "steelblue");
             index++;
         } else {
             clearInterval(interval);
-            d3.select("#scene1").append("button").text("Next").on("click", () => showScene("#scene2"));
         }
     }, 500);
+
+    d3.select("#next1").on("click", () => showScene("#scene2"));
 }
 
 function initializeScene2(data) {
-    const svg = d3.select("#scene2").append("svg").attr("width", 800).attr("height", 600);
-    // Sort data by RPDI
-    data.sort((a, b) => b.RPDI - a.RPDI);
-    // Create bars
-    // Animate timeline
+    const svg = d3.select("#scene2").append("svg");
+    const margin = {top: 20, right: 30, bottom: 40, left: 90},
+          width = 800 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+
+    const g = svg.append("g")
+                 .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    data.sort((a, b) => b.value - a.value);
+
+    const x = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.value)])
+                .range([0, width]);
+
+    const y = d3.scaleBand()
+                .domain(data.map(d => d.country))
+                .range([0, height])
+                .padding(0.1);
+
+    g.append("g")
+     .attr("class", "x axis")
+     .attr("transform", `translate(0,${height})`)
+     .call(d3.axisBottom(x));
+
+    g.append("g")
+     .attr("class", "y axis")
+     .call(d3.axisLeft(y));
+
     let index = 0;
     const interval = setInterval(() => {
         if (index < data.length) {
             const d = data[index];
-            svg.append("rect")
-                .attr("x", 100)
-                .attr("y", index * 20)
-                .attr("width", d.RPDI * 10)
-                .attr("height", 15)
-                .attr("fill", "orange");
-            svg.append("text")
-                .attr("x", 10)
-                .attr("y", index * 20 + 12)
-                .text(d.Country);
+            g.append("rect")
+             .attr("x", x(0))
+             .attr("y", y(d.country))
+             .attr("width", x(d.value))
+             .attr("height", y.bandwidth())
+             .attr("fill", "orange");
             index++;
         } else {
             clearInterval(interval);
-            d3.select("#scene2").append("button").text("Next").on("click", () => showScene("#scene3"));
         }
     }, 500);
+
+    d3.select("#next2").on("click", () => showScene("#scene3"));
 }
 
-function initializeScene3(dataRHPI, dataRPDI) {
-    const svg = d3.select("#scene3").append("svg").attr("width", 800).attr("height", 600);
-    // Create scatter plot
-    const data = dataRHPI.map((d, i) => ({
-        Country: d.Country,
-        RHPI: +d.RHPI,
-        RPDI: +dataRPDI[i].RPDI
+function initializeScene3(rhpiData, rpdiData) {
+    const svg = d3.select("#scene3").append("svg");
+    const margin = {top: 20, right: 30, bottom: 40, left: 90},
+          width = 800 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+
+    const g = svg.append("g")
+                 .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const data = rhpiData.map((d, i) => ({
+        country: d.country,
+        RHPI: d.value,
+        RPDI: rpdiData[i].value
     }));
 
-    const xScale = d3.scaleLinear().domain([0, d3.max(data, d => d.RHPI)]).range([100, 700]);
-    const yScale = d3.scaleLinear().domain([0, d3.max(data, d => d.RPDI)]).range([500, 100]);
+    const x = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.RHPI)])
+                .range([0, width]);
 
-    svg.append("g").attr("transform", "translate(0,500)").call(d3.axisBottom(xScale));
-    svg.append("g").attr("transform", "translate(100,0)").call(d3.axisLeft(yScale));
+    const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.RPDI)])
+                .range([height, 0]);
 
-    svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xScale(d.RHPI))
-        .attr("cy", d => yScale(d.RPDI))
-        .attr("r", 5)
-        .attr("fill", "green");
+    g.append("g")
+     .attr("class", "x axis")
+     .attr("transform", `translate(0,${height})`)
+     .call(d3.axisBottom(x));
 
-    svg.selectAll("text.label")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("x", d => xScale(d.RHPI))
-        .attr("y", d => yScale(d.RPDI))
-        .attr("dx", 7)
-        .attr("dy", -7)
-        .attr("font-size", "10px")
-        .text(d => d.Country);
+    g.append("g")
+     .attr("class", "y axis")
+     .call(d3.axisLeft(y));
+
+    g.selectAll("circle")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("cx", d => x(d.RHPI))
+     .attr("cy", d => y(d.RPDI))
+     .attr("r", 5)
+     .attr("fill", "green");
+
+    g.selectAll("text.label")
+     .data(data)
+     .enter()
+     .append("text")
+     .attr("x", d => x(d.RHPI))
+     .attr("y", d => y(d.RPDI))
+     .attr("dx", 7)
+     .attr("dy", -7)
+     .attr("font-size", "10px")
+     .text(d => d.country);
+
+    d3.select("#start-over").on("click", () => showScene("#scene1"));
 }
 
 function showScene(sceneId) {
